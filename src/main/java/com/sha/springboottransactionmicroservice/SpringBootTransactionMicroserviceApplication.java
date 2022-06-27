cca4c7cf6f68f72311b52d902825ea8ac1cc8f76
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-28 02:33:56 +0300

    Initial commit

---
+++ src/main/java/com/sha/springboottransactionmicroservice/SpringBootTransactionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sha.springboottransactionmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransactionMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTransactionMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sha/springboottransactionmicroservice/SpringBootTransactionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.springboottransactionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTransactionMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
