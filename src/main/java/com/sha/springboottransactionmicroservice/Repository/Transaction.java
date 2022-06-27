dab0d31da40e81a526ab83ed6e19d4dccbe96e64
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-28 02:45:46 +0300

    add missed annotation

--- src/main/java/com/sha/springboottransactionmicroservice/Repository/Transaction.java
+++ src/main/java/com/sha/springboottransactionmicroservice/Repository/Transaction.java
@@ -1,9 +1,11 @@
 package com.sha.springboottransactionmicroservice.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface Transaction extends JpaRepository<Transaction, Long> {
    List<Transaction> findAllByUserId(Long userId);
 }
