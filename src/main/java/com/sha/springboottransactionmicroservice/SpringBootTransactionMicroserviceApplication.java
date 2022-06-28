0b99b731b7e71ffdf7509f9fc83dd40934676585
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-28 15:35:34 +0300

    set property source

--- src/main/java/com/sha/springboottransactionmicroservice/SpringBootTransactionMicroserviceApplication.java
+++ src/main/java/com/sha/springboottransactionmicroservice/SpringBootTransactionMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.sha.springboottransactionmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootTransactionMicroserviceApplication {
 
     public static void main(String[] args) {
