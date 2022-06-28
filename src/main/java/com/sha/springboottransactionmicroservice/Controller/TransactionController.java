f5ac6802d9899453f056f4a8767ad96f78ab42ca
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-28 03:04:46 +0300

    set transaction controller class operations

---
+++ src/main/java/com/sha/springboottransactionmicroservice/Controller/TransactionController.java
@@ -0,0 +1,33 @@
+package com.sha.springboottransactionmicroservice.Controller;
+
+import com.sha.springboottransactionmicroservice.Model.Transaction;
+import com.sha.springboottransactionmicroservice.Service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/transaction")
+public class TransactionController {
+  @Autowired
+  private TransactionService transactionService;
+
+  @PostMapping
+  public ResponseEntity<?> saveTransaction(@RequestBody Transaction transaction) {
+     return new ResponseEntity<>(transactionService.saveTransaction(transaction),
+             HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/{transactionId}")
+  public ResponseEntity<?> deleteTransaction(@PathVariable Long transactionId) {
+     transactionService.deleteTransaction(transactionId);
+     return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<?> getAllTransactionsOfUser(@PathVariable Long userId) {
+     return new ResponseEntity<>(transactionService.findAllTransactionsByUserId(userId),
+             HttpStatus.OK);
+  }
+}
